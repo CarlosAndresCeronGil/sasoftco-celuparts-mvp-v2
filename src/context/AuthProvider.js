2130f78f0333e93364884e0496ec534eb9957ea0
CarlosAndresCeronGil <andrescg0106@hotmail.com>
2022-09-16 09:08:09 -0500

    Agregacion del context
    Agregación del context, critico para el manejo de sesión de usuari

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,13 @@
+import React, { useState, createContext } from "react";
+
+const AuthContext = createContext({});
+
+export function AuthProvider({children}) {
+    const [auth, setAuth] = useState({});
+
+    return <AuthContext.Provider value={{ auth, setAuth }}>
+        {children}
+    </AuthContext.Provider>
+}
+
+export default AuthContext;
