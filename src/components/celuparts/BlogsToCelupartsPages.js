3028cde37661bf55ede0db2a2cd973b4282f454a
CarlosAndresCeronGil <andrescg0106@hotmail.com>
2022-09-23 14:01:37 -0500

    Cambios para que se vean distintos los mensajes del dashboard segun el rol

--- src/components/celuparts/BlogsToCelupartsPages.js
+++ src/components/celuparts/BlogsToCelupartsPages.js
@@ -14,9 +14,9 @@ const cards = [
     img: img1,
     title: 'Mis reparaciones.',
     subtitle: 'Lista de tus reparaciones registradas en el sistema, podrás aceptar o rechazar las cotizaciones que se asignen a tus productos.',
-    role: [ 'admin', 'tecnico', 'mensajero', 'user', ],
+    role: ['admin', 'tecnico', 'mensajero', 'user',],
     href: [
-    	'/home/repair-requests-table',
+      '/home/repair-requests-table',
       '/home/repair-requests-table',
       '/home/repair-requests-table',
       '/home/user-repair-requests',
@@ -26,7 +26,7 @@ const cards = [
     img: img2,
     title: 'Mis retomas.',
     subtitle: 'Lista de tus retomas registradas, donde podras aceptar o rechazar el valor al que te ofrecemos vender tu dispositivo.',
-    role: [ 'admin', 'tecnico', 'mensajero', 'user', ],
+    role: ['admin', 'tecnico', 'mensajero', 'user',],
     href: [
       '/home/retoma-requests-table',
       '/home/retoma-requests-table',
@@ -38,9 +38,48 @@ const cards = [
     img: img3,
     title: 'Mis notificaciones',
     subtitle: 'Página en donde podras ver tus notificaciones referentes a tus reparaciones o retomas activas en el sistema.',
-    role: [ 'admin', 'tecnico', 'mensajero', 'user', ],
+    role: ['admin', 'tecnico', 'mensajero', 'user',],
     href: [
-    	'/home/admin-alerts',
+      '/home/admin-alerts',
+      '/home/technician-alerts',
+      '/home/courier-alerts',
+      '/home/user-alerts',
+    ],
+  },
+];
+
+const cardsForAdmins = [
+  {
+    img: img1,
+    title: 'Lista de reparaciones.',
+    subtitle: 'Lista de las reparaciones registradas en el sistema, donde podrás actualizar los estados de estas.',
+    role: ['admin', 'tecnico', 'mensajero', 'user',],
+    href: [
+      '/home/repair-requests-table',
+      '/home/repair-requests-table',
+      '/home/repair-requests-table',
+      '/home/user-repair-requests',
+    ],
+  },
+  {
+    img: img2,
+    title: 'Lista de retomas.',
+    subtitle: 'Lista de las retomas registradas en el sistema. Al igual que la lista de reparacione podras actualizar los estados de estos procesos.',
+    role: ['admin', 'tecnico', 'mensajero', 'user',],
+    href: [
+      '/home/retoma-requests-table',
+      '/home/retoma-requests-table',
+      '/home/retoma-requests-table',
+      '/home/user-retoma-requests',
+    ],
+  },
+  {
+    img: img3,
+    title: 'Mis notificaciones',
+    subtitle: 'Página en donde podras ver tus notificaciones referentes a procesos nuevos o actualizaciones de los clientes.',
+    role: ['admin', 'tecnico', 'mensajero', 'user',],
+    href: [
+      '/home/admin-alerts',
       '/home/technician-alerts',
       '/home/courier-alerts',
       '/home/user-alerts',
@@ -55,25 +94,54 @@ const style = {
 const BlogsToCelupartsPages = () => {
 
   const currentRole = JSON.parse(localStorage.getItem('user')).role;
-  
+
   return (
-    <Row>
-      {
-        cards.map(( { img, title, subtitle, role, href } ) => (
-          <Col lg="4" key={ title }>
-            <Card className='hover-zoom mt-3 img-wrapper' style={{ height: "340px" }} >
-              <img src={ img } alt={ img } className='mx-3 rounded mt-n3 img-fluid' />
-              <Link to={ href[ role.indexOf( currentRole ) ] } style={{ textDecoration: 'none',  }}>
-                <CardBody style={ style }>
-                  <CardTitle className='mt-2' tag="h4">{ title }</CardTitle>
-                  <CardSubtitle className="text-muted mt-1">{ subtitle }</CardSubtitle>
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-        ))
-      }
-    </Row>
+    JSON.parse(localStorage.getItem('user')).role === "admin"
+      ||
+      JSON.parse(localStorage.getItem('user')).role === "tecnico"
+      ||
+      JSON.parse(localStorage.getItem('user')).role === "tecnico"
+      ?
+      (
+        <Row>
+          {
+            cardsForAdmins.map(({ img, title, subtitle, role, href }) => (
+              <Col lg="4" key={title}>
+                <Card className='hover-zoom mt-3 img-wrapper' style={{ height: "340px" }} >
+                  <img src={img} alt={img} className='mx-3 rounded mt-n3 img-fluid' />
+                  <Link to={href[role.indexOf(currentRole)]} style={{ textDecoration: 'none', }}>
+                    <CardBody style={style}>
+                      <CardTitle className='mt-2' tag="h4">{title}</CardTitle>
+                      <CardSubtitle className="text-muted mt-1">{subtitle}</CardSubtitle>
+                    </CardBody>
+                  </Link>
+                </Card>
+              </Col>
+            ))
+          }
+        </Row>
+      ) :
+      (
+        <Row>
+          {
+            cards.map(({ img, title, subtitle, role, href }) => (
+              <Col lg="4" key={title}>
+                <Card className='hover-zoom mt-3 img-wrapper' style={{ height: "340px" }} >
+                  <img src={img} alt={img} className='mx-3 rounded mt-n3 img-fluid' />
+                  <Link to={href[role.indexOf(currentRole)]} style={{ textDecoration: 'none', }}>
+                    <CardBody style={style}>
+                      <CardTitle className='mt-2' tag="h4">{title}</CardTitle>
+                      <CardSubtitle className="text-muted mt-1">{subtitle}</CardSubtitle>
+                    </CardBody>
+                  </Link>
+                </Card>
+              </Col>
+            ))
+          }
+        </Row>
+      )
+
+
   );
 };
 
