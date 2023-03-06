2f332d84abfa140d144626bda7b158dfd07abdb7
Diego Rojas <Diego.Cabrera@sasoftco.com>
2023-03-06 11:13:11 -0500

    change extension downloades file

--- src/services/getEquipmentInvoice.js
+++ src/services/getEquipmentInvoice.js
@@ -1,24 +1,26 @@
-import { API_URL } from "./settings";
+import { API_URL } from './settings';
 
 export default function getEquipmentInvoice({ id }) {
-    const apiURL = `${API_URL}/Equipment/downloadFile/${id}`;
-    return fetch(apiURL)
-        // .then((response) => response.blob())
-        // .then(blob => {
-        //     const file = window.URL.createObjectURL(blob);
-        //     window.location.assign(file)
-        // })
-        // .catch(error => {
-        //     console.log(error)
-        // })
-        .then(response => response.blob())
-        .then(blob => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = "factura.pdf";
-            document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-            a.click();    
-            a.remove();  //afterwards we remove the element again         
-        });
+  const apiURL = `${API_URL}/Equipment/downloadFile/${id}`;
+  return (
+    fetch(apiURL)
+      // .then((response) => response.blob())
+      // .then(blob => {
+      //     const file = window.URL.createObjectURL(blob);
+      //     window.location.assign(file)
+      // })
+      // .catch(error => {
+      //     console.log(error)
+      // })
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `factura.${blob.type.slice(blob.type.indexOf('/') + 1)}`;
+        document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+        a.click();
+        a.remove(); //afterwards we remove the element again
+      })
+  );
 }
